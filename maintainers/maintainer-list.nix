5d056bf05864fb7fceb6c6452287b342b5c225ab
Viktor Illmer <1476338+v-ji@users.noreply.github.com>
2024-07-07 13:55:25 +0200

    maintainers: add vji

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21148,6 +21148,12 @@
     githubId = 7444430;
     name = "Radik Islamov";
   };
+  vji = {
+    email = "mail@viktor.im";
+    github = "v-ji";
+    githubId = 1476338;
+    name = "Viktor Illmer";
+   };
   vklquevs = {
     email = "vklquevs@gmail.com";
     github = "vklquevs";
